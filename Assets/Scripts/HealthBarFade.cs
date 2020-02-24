e344fe88ad4dbfd2e007ddfc1c57c1ccde357fce
Mohamed R. Khan <khanantisora@hotmail.com>
2020-02-24 18:11:48 -0500

    HealthBar Created
    The actual bar and basic logic created. Now to work on actually losing/gaining health for later.

---
+++ Assets/Scripts/HealthBarFade.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarFade : MonoBehaviour
+{
+    private Image barImage;
+
+    private void Awake()
+    {
+        barImage = transform.Find("Bar").GetComponent<Image>();
+    }
+
+    private void Start()
+    {
+        setHealth(.8f);
+    }
+
+    private void setHealth(float healthNormalized)
+    {
+        barImage.fillAmount = healthNormalized;
+    } 
+}
