aec6286a151f84eb239f151e046d7f9ba01e2006
Mohamed R. Khan <khanantisora@hotmail.com>
2020-02-24 23:46:28 -0500

    Player HealthBar Functions Completed
    The health bar functions correctly (Functions are in place). Next we need to add some animations/ have the enemies damage the player. Going to work on that tomorrow.

---
+++ Assets/Scripts/HealthBarAni.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarAni : MonoBehaviour
+{
+    private HealthSystemPlayer healthSystem;
+    private Image barImage;
+
+    private void Awake()
+    {
+        barImage = transform.Find("Bar").GetComponent<Image>();
+    }
+
+    private void Start()
+    {
+        healthSystem = new HealthSystemPlayer(100);
+        setHealth(healthSystem.GetHealthNormalized());
+        healthSystem.onDamaged += HealthSystem_onDamaged;
+        healthSystem.onHealed += HealthSystem_onHealed;
+
+        
+    }
+    
+
+    private void HealthSystem_onHealed(object sender, System.EventArgs e)
+    {
+        setHealth(healthSystem.GetHealthNormalized());
+       // throw new System.NotImplementedException();
+    }
+
+    private void HealthSystem_onDamaged(object sender, System.EventArgs e)
+    {
+        setHealth(healthSystem.GetHealthNormalized());
+        // throw new System.NotImplementedException();
+    }
+
+    private void setHealth(float healthNormalized)
+    {
+        barImage.fillAmount = healthNormalized;
+    } 
+}
---
+++ Assets/Scripts/HealthSystemPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class HealthSystemPlayer : MonoBehaviour
+{
+    public event EventHandler onDamaged;
+    public event EventHandler onHealed;
+
+    private int health;
+    private int healthMax;
+
+    public HealthSystemPlayer(int health) { //Constructor for the health system.
+        healthMax = health;
+        this.health = health;
+    }
+
+    public int GetHealth() //Getter function if we need it.
+    {
+        return health; 
+    }
+
+    public void Damage(int dmgAmount) //Function to take damage.
+    {
+        health -= dmgAmount;
+        if (health < 0) health = 0; //Makes it so health can't drop below 0 into negatives.
+        if (onDamaged != null) onDamaged(this, EventArgs.Empty);
+    }
+
+    public void Heal(int healAmount)
+    {
+        health += healAmount;
+        if (health > healthMax) health = healthMax;
+        if (onHealed != null) onHealed(this, EventArgs.Empty);
+    }
+
+    public float GetHealthNormalized() //This returns the health in percentage form.
+    {
+        return (float)health / healthMax;
+    }
+
+}
