1f4fdfeeca1959c643e16c9a976b457116366429
Oris Feliz <felizoris@gmail.com>
2020-03-01 17:28:12 -0500

    made everything smaller, created dedicated enemy script, began implementing enemy manager.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,8 +19,8 @@ public class Player : MonoBehaviour
     private float       myTime = 0f;
 
     int                 bultype = 1;
-    public Vector3      bullet_rotation1;
-    public Vector3      bullet_rotation2;
+    Vector3      bullet_rotation1;
+    Vector3      bullet_rotation2;
     public Text         shot_Text;
 
     //Invincibility after hit
@@ -36,11 +36,17 @@ public class Player : MonoBehaviour
         rend = GetComponent<Renderer>();
         color = rend.material.color;
         gameOverPrefab.SetActive(false);
+        bullet_rotation1 = new Vector3(0, 0, 12);
+        bullet_rotation2 = new Vector3(0, 0, -12);
     }
     void FixedUpdate()
     {
         Movement();
         shoot();
+
+        transform.position = new Vector2(
+            Mathf.Clamp(transform.position.x, -7.7f, 4.7f),
+            Mathf.Clamp(transform.position.y, -4.2f, 4.2f));
     }
 
     //====================================
@@ -128,7 +134,7 @@ public class Player : MonoBehaviour
     }
 
     //Invincibility state when getting hit.
-    IEnumerator PlayerInvince()
+    public IEnumerator PlayerInvince()
     {
         Physics2D.IgnoreLayerCollision(0, 10, true);
         color.a = 0.5f;
---
+++ Assets/Scripts/enemy/enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public GameObject enemyManager;
+    public float speed = 2;
+
+    private void Start()
+    {
+        enemyManager = GameObject.Find("EnemyManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (collision.tag == "player_shot")
+        {
+            ScoreCount.scoreValue += 10;
+            Destroy(this.gameObject);
+        }
+
+        if (collision.tag == "Despawner")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
