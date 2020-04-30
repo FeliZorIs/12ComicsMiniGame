dbb29ca6b9129b9b1207b4beb9e28a40d3bf0409
Krishan Patel <50147772+kpatel-1@users.noreply.github.com>
2020-04-30 02:50:17 -0400

    How To Play buttons/update, Button updates on leaderboards/customize, Script Cleanup

--- Assets/Scripts/Menus/HowToPlay.cs
+++ Assets/Scripts/Menus/HowToPlay.cs
@@ -17,6 +17,11 @@ public class HowToPlay : MonoBehaviour
     public GameObject P4;
     public GameObject P5;
 
+    public Button LeftBtn;
+    public Button RightBtn;
+    public Button XBtn;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -37,104 +42,121 @@ public class HowToPlay : MonoBehaviour
             if (P1.activeSelf) {
                 if (Input.GetKeyDown("right"))
                 {
-                    P1.SetActive(false);
-                    P2.SetActive(true);
-                    P3.SetActive(false);
-                    P4.SetActive(false);
-                    P5.SetActive(false);
+                    PG2();
                 }
+                RightBtn.onClick.AddListener(PG2);
             }
 
             else if (P2.activeSelf)
             {
                 if (Input.GetKeyDown("right"))
                 {
-                    P1.SetActive(false);
-                    P2.SetActive(false);
-                    P3.SetActive(true);
-                    P4.SetActive(false);
-                    P5.SetActive(false);
+                    PG3();
                 }
 
                 if (Input.GetKeyDown("left"))
                 {
-                    P1.SetActive(true);
-                    P2.SetActive(false);
-                    P3.SetActive(false);
-                    P4.SetActive(false);
-                    P5.SetActive(false);
+                    PG1();
                 }
+                RightBtn.onClick.AddListener(PG3);
+                LeftBtn.onClick.AddListener(PG1);
             }
 
             else if (P3.activeSelf)
             {
                 if (Input.GetKeyDown("right"))
                 {
-                    P1.SetActive(false);
-                    P2.SetActive(false);
-                    P3.SetActive(false);
-                    P4.SetActive(true);
-                    P5.SetActive(false);
+                    PG4();
                 }
 
                 if (Input.GetKeyDown("left"))
                 {
-                    P1.SetActive(false);
-                    P2.SetActive(true);
-                    P3.SetActive(false);
-                    P4.SetActive(false);
-                    P5.SetActive(false);
+                    PG2();
                 }
+                RightBtn.onClick.AddListener(PG4);
+                LeftBtn.onClick.AddListener(PG2);
             }
 
             else if (P4.activeSelf)
             {
                 if (Input.GetKeyDown("right"))
                 {
-                    P1.SetActive(false);
-                    P2.SetActive(false);
-                    P3.SetActive(false);
-                    P4.SetActive(false);
-                    P5.SetActive(true);
+                    PG5();
                 }
 
                 if (Input.GetKeyDown("left"))
                 {
-                    P1.SetActive(false);
-                    P2.SetActive(false);
-                    P3.SetActive(true);
-                    P4.SetActive(false);
-                    P5.SetActive(false);
+                    PG3();
                 }
+                RightBtn.onClick.AddListener(PG5);
+                LeftBtn.onClick.AddListener(PG3);
             }
 
             else if (P5.activeSelf)
             {
                 if (Input.GetKeyDown("left"))
                 {
-                    P1.SetActive(false);
-                    P2.SetActive(false);
-                    P3.SetActive(false);
-                    P4.SetActive(true);
-                    P5.SetActive(false);
+                    PG4();
                 }
+                LeftBtn.onClick.AddListener(PG4);
             }
-
-
-            if (Input.GetKey("escape"))
-            {
-                How2Play.SetActive(false);
-                P1.SetActive(true);
-                P2.SetActive(false);
-                P3.SetActive(false);
-                P4.SetActive(false);
-                P5.SetActive(false);
-            }
+            XBtn.onClick.AddListener(iEsc);
         }
     }
 
     public void H2P()
     {
         How2Play.SetActive(true);
+    }
+
+
+    public void PG1()
+    {
+        P1.SetActive(true);
+        P2.SetActive(false);
+        P3.SetActive(false);
+        P4.SetActive(false);
+        P5.SetActive(false);
+    }
+    public void PG2()
+    {
+        P1.SetActive(false);
+        P2.SetActive(true);
+        P3.SetActive(false);
+        P4.SetActive(false);
+        P5.SetActive(false);
+    }
+    public void PG3()
+    {
+        P1.SetActive(false);
+        P2.SetActive(false);
+        P3.SetActive(true);
+        P4.SetActive(false);
+        P5.SetActive(false);
+    }
+    public void PG4()
+    {
+        P1.SetActive(false);
+        P2.SetActive(false);
+        P3.SetActive(false);
+        P4.SetActive(true);
+        P5.SetActive(false);
+    }
+    public void PG5()
+    {
+        P1.SetActive(false);
+        P2.SetActive(false);
+        P3.SetActive(false);
+        P4.SetActive(false);
+        P5.SetActive(true);
+    }
+    public void iEsc()
+    {
+        How2Play.SetActive(false);
+        P1.SetActive(true);
+        P2.SetActive(false);
+        P3.SetActive(false);
+        P4.SetActive(false);
+        P5.SetActive(false);
     }
 }
